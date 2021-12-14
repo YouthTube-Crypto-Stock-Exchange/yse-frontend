b8e46b4765fabae0605f1d937a0a1d03335f40da
Crehan <crehanas14@gmail.com>
2021-12-14 19:36:17 +0530

    add holdings page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './components/Navbar';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Route, Routes } from 'react-router-dom';
+import { Nav , Navbar , Container } from 'react-bootstrap';
+import Holdings from './components/Holdings';
 
 function App() {
 	const { isAuthenticated, loginWithRedirect, user } = useAuth0();
@@ -16,16 +17,26 @@ function App() {
 			/>
 			<Route
 				path='/holdings'
-				element={<h1>Holdngs</h1> /* Add Holdings component here */}
+				element = {<Holdings />}
 			/>
 			<Route
 				path='/dashboard'
-				element={<h1>Dashboard</h1> /* Add Dashboard component here */}
+				element={<h1>Dashboard</h1> /* Add Holdings component here */}
 			/>
 		</Routes>
 	);
 	return (
 		<>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">YOUTHtube Stock Exchange</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="holdings">Holdings</Nav.Link>
+                        <Nav.Link href="dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="createITO">ITO</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
 			{routes}
 			{!isAuthenticated ? (
 				<button onClick={() => loginWithRedirect()}>Login</button>
