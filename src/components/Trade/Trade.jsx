775570dbc10c64ad18e4af14b2074642b5b5456c
Crehan <crehanas14@gmail.com>
2021-12-24 00:15:08 +0530

    add buy/sell form

--- src/components/Trade/Trade.jsx
+++ src/components/Trade/Trade.jsx
@@ -1,27 +1,40 @@
 import { useEffect, useState } from 'react';
 import { withRouter } from 'react-router';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Container, Row, Col, Table, Button, Modal, Card } from 'react-bootstrap';
+import { Container, Row, Col, Table, Button, Modal, Card , Form , InputGroup } from 'react-bootstrap';
 import Linechart from '../Linechart/Linechart';
+
+const buy = "Buy Shares";
+const sell = "Sell Shares";
+const aggressive = "Aggresive Bid";
+const normal = "Normal Bid";
 
 const Trade = props => {
 	const [pageFound, setPageFound] = useState(true);
 	const [influencer, setInfluencer] = useState({});
+
+    const [ action , setAction ] = useState(buy);
+    const [ shareCount , setShareCount ] = useState();
+    const [ sharePrice , setSharePrice ] = useState();
+    const [ bidType , setBidType ] = useState(normal);
+
 	const searchValue = new URLSearchParams(props.location.search).get(
 		'search'
 	);
+	const { user } = useAuth0();
+
 	useEffect(() => {
-		// fetch(`http://localhost:8080/getInfluencerDetails/${searchValue}`)
-		// 	.then(response => response.json())
-		// 	.then(data => {
-		// 		if (data.msg === 'Influencer Not Found') {
-		// 			setPageFound(false);
-		// 		} else {
-		// 			setInfluencer(data.influencer);
-		// 			setPageFound(true);
-		// 		}
-		// 	});
+		console.log(user);
+        let path = 'http://localhost:8080/getInfluencerDetails/' + searchValue;
+		fetch(path)
+			.then(response => response.json())
+			.then(data => {
+                console.log(data);
+				if (data.msg==='Influencer Not Found') setPageFound(false);
+				else setPageFound(true);
+			});
 	}, [searchValue]);
+
 	return (
 		<>
 			{pageFound ? (
@@ -55,12 +68,96 @@ const Trade = props => {
 					<br />
 					<br />
 					<Row className='align-items-center'>
-						<Col className='d-flex justify-content-end'>
-							<Button variant='success'>Buy</Button>
-						</Col>
-						<Col className='d-flex justify-content-start'>
-							<Button variant='danger'>Sell</Button>
-						</Col>
+                        <Container className="d-flex flex-column align-items-center">
+                            <Card className="shadow w-50 mt-5">
+                                <Card.Body className='px-5'>
+                                    <Form>
+                                        <div key={'inline-radio'} className="my-3">
+                                        <Form.Check
+                                            inline
+                                            label="Buy Shares"
+                                            name="group1"
+                                            type="radio"
+                                            onClick={()=>{setAction(buy);}}
+                                            id='inline-radio-1'
+                                        />
+                                        <Form.Check
+                                            inline
+                                            label="Sell Shares"
+                                            name="group1"
+                                            type="radio"
+                                            id='inline-radio-2'
+                                            onClick={()=>{setAction(sell)}}
+                                        />
+                                        </div>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        </Container>
+                        <Container className="d-flex flex-column align-items-center mb-5">
+                            <Card className="shadow w-50 mt-5">
+                                <Card.Header className='pt-3 pb d-flex flex-column align-items-center bg-dark text-white'>
+                                    <h2><strong>{ action }</strong></h2>
+                                </Card.Header>
+                                <Card.Body className='px-5'>
+                                    <Form>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label><strong>Number of Shares</strong></Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Enter the Number of Shares"
+                                                value={shareCount}
+                                                onChange={(event) => {setShareCount(event.target.value)}}
+                                            />
+                                        </Form.Group>
+
+                                        <div key={'inline-radio'} className="my-3">
+                                        <Form.Check
+                                            inline
+                                            label={normal}
+                                            name="group1"
+                                            type="radio"
+                                            id='inline-radio-1'
+                                            onClick={() => {
+                                                setBidType(normal)
+                                                setSharePrice('');
+                                            }}
+                                        />
+                                        <Form.Check
+                                            inline
+                                            label={aggressive}
+                                            name="group1"
+                                            type="radio"
+                                            id='inline-radio-2'
+                                            onClick={() => {
+                                                setBidType(aggressive)
+                                                setSharePrice(-1);
+                                            }}
+                                        />
+                                        </div>
+
+                                        <Form.Group className="mb-3">
+                                            <Form.Label><strong>Price per share</strong></Form.Label>
+                                            <InputGroup>
+                                                <Form.Control
+                                                    type="text"
+                                                    placeholder="Enter Price per share"
+                                                    value={(bidType===aggressive)?'':sharePrice}
+                                                    onChange={(event) => {setSharePrice(event.target.value)}}
+                                                    disabled={(bidType===aggressive)?true:false}
+                                                />
+                                                <InputGroup.Text id="basic-addon2">Youth Tokens</InputGroup.Text>
+                                            </InputGroup>
+                                        </Form.Group>
+                                        <div className='d-flex flex-column align-items-center'>
+                                        <Button variant="primary" type="submit">
+                                            Submit
+                                        </Button>
+                                        </div>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        </Container>
 					</Row>
 				</Container>
 			) : (
