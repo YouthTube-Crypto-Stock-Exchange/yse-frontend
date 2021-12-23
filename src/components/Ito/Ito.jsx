27e3f0a0533712b286d6ebaf2367d31f9c43c4c5
Crehan <crehanas14@gmail.com>
2021-12-23 18:21:16 +0530

    add ito creation logic

--- src/components/Ito/Ito.jsx
+++ src/components/Ito/Ito.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
-import { Form , Button, Card, Container } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import web3 from '../../web3';
+import { Form , Button, Card, Container , InputGroup } from "react-bootstrap";
+import Spinner from '../Spinner/Spinner';
+import contract from '../../contract';
+
 const Ito = () => {
     const [ shareCount , setShareCount ] = useState();
     const [ sharePrice , setSharePrice ] = useState();
+    const [ isLoading , setIsLoading ] = useState(false);
+    const [ isInfluencer , setIsInfluencer ] = useState(false);
+    const { user } = useAuth0();
 
-	const handleSubmit = (e) => {
+    useEffect( () => {
+        console.log(user.sub);
+		fetch(`http://localhost:8080/getUserDetails/${user.sub}`)
+			.then(response => response.json())
+			.then(async data => {
+                console.log(data);
+                if(data.user.isInfluencer){
+                    console.log("user is already an influencer");
+                    setIsInfluencer(true);
+                }
+			})
+    },[])
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+        if(!isInfluencer){
+            console.log('creating ITO');
+            const accounts = await web3.eth.getAccounts();
+            await contract.methods
+                .ITO(shareCount, sharePrice, user.name)
+                .send({
+                    from: accounts[0],
+                });
+            console.log('ITO created');
+        }
 		setSharePrice('');
 		setShareCount('');
 	};
@@ -31,12 +62,15 @@ const Ito = () => {
 
                     <Form.Group className="mb-3">
                         <Form.Label><strong>Price per share</strong></Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter Price per share"
-                            value={sharePrice}
-                            onChange={(event) => {setSharePrice(event.target.value)}}
-                        />
+                        <InputGroup>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter Price per share"
+                                value={sharePrice}
+                                onChange={(event) => {setSharePrice(event.target.value)}}
+                            />
+                            <InputGroup.Text id="basic-addon2">Youth Tokens</InputGroup.Text>
+                        </InputGroup>
                     </Form.Group>
                     <div className='d-flex flex-column align-items-center'>
                     <Button variant="primary" type="submit">
